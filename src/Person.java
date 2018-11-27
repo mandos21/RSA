db0f85845c89fdfc5e7be6a172d41d2fb9fe6852
Matthew DeGenaro <mt.degenaro@gmail.com>
2018-11-27 14:34:38 -0500

    added framework for methods and constructors

---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person
+{
+  public person()
+  {
+
+  }
+
+  public long getM()
+  {
+    return null;
+  }
+
+  public long getE()
+  {
+    return null;
+  }
+
+  public long[] encryptTo(java.lang.String msg, Person she)
+  {
+    return null;
+  }
+
+  public java.lang.String decrypt(long[] cipher)
+  {
+    return null;
+  }
+
+
+}
