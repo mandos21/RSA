26d7c5bb76e1a609c08ff92eaa6a4fca32ce2a7d
Matthew DeGenaro <mt.degenaro@gmail.com>
2018-11-27 14:38:18 -0500

    added framework for methods and constructors

---
+++ src/RSA.java
@@ -0,0 +1,47 @@
+public class RSA
+{
+  public RSA()
+  {
+
+  }
+
+  public static void main(java.lang.String[] args)
+  {
+
+  }
+
+  public static long inverse(long e, long m)
+  {
+    return null;
+  }
+
+  public static void show(long[] cipher)
+  {
+
+  }
+
+  public static long modPower(long b, long p, long m)
+  {
+    return null;
+  }
+
+  public static long randPrime(int m, int n, java.util.Random rand)
+  {
+    return null;
+  }
+
+  public static long relPrime(long n, java.util.Random rand)
+  {
+    return null;
+  }
+
+  public static long toLong(java.lang.String msg, int p)
+  {
+    return null;
+  }
+
+  public static java.lang.String longTo2Chars(long x)
+  {
+    return null;
+  }
+}
