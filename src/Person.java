4335a07cf60fb2a3bb0be8a51639face38e133b4
Matthew DeGenaro <mt.degenaro@gmail.com>
2018-12-02 23:38:30 -0500

    Finished constructor, getM, getE methods

--- src/Person.java
+++ src/Person.java
@@ -5,29 +5,44 @@ public class Person
   private long e;
   private long m;
   private long d;
+  private long p;
+  private long q;
+  private long n;
 
 
   public Person()
   {
 
-    Random rand = new Random();
-    e = RSA.randPrime(Integer.MAX_VALUE/4,Integer.MAX_VALUE, rand);
-    m = RSA.randPrime();
+    p = RSA.randPrime(Integer.MAX_VALUE/4,Integer.MAX_VALUE, new Random());
+    q = RSA.randPrime(Integer.MAX_VALUE/4,Integer.MAX_VALUE, new Random());  
+    m = p*q;
+    n = (p-1)*(q-1);
+    e = RSA.relPrime(n, new Random());
+    d = RSA.inverse(e,m);
+    
   }
 
   public long getM()
   {
-    return null;
+    return m;
   }
 
   public long getE()
   {
-    return null;
+    return e;
   }
 
   public long[] encryptTo(java.lang.String msg, Person she)
   {
-    return null;
+	  long[] returnLong = [msg.length()];
+	  
+	  for()
+	  {
+		  
+	  }
+	  
+	  
+    return RSA.modPower(msg., she.e, she.m);
   }
 
   public java.lang.String decrypt(long[] cipher)
