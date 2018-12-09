9de9dc24006b784ac2de57357c3fe744a21e4218
Matthew DeGenaro <mt.degenaro@gmail.com>
2018-12-09 17:36:20 -0500

    Fixed Merge Conflicts

--- src/RSA.java
+++ src/RSA.java
@@ -11,51 +11,45 @@ public class RSA
 {
 	public static void main (String args[])
 	{
-<<<<<<< HEAD
-
 		Person Alice = new Person();
-=======
-		
+
+
 		long x = 345; //17
 		long y = 76408; //75
-		
+
 		long tester = RSA.inverse(x, y);
 		System.out.println(tester);
-		
-		/**Person Alice = new Person();
->>>>>>> origin/Moore
+
+		Person Alice = new Person();
+
 		Person Bob = new Person();
-	
+
 		String msg = new String ("Bob, let's have lunch."); 	// message to be sent to Bob
 		long []  cipher;
 		cipher =  Alice.encryptTo(msg, Bob);			// encrypted, with Bob's public key
-	
+
 		System.out.println ("Message is: " + msg);
 		System.out.println ("Alice sends:");
 		show (cipher);
-	
+
 		System.out.println ("Bob decodes and reads: " + Bob.decrypt (cipher));	// decrypted,
 									// with Bob's private key.
 		System.out.println ();
-	
+
 		msg = new String ("No thanks, I'm busy");
 		cipher = Bob.encryptTo (msg, Alice);
-	
+
 		System.out.println ("Message is: " + msg);
 		System.out.println ("Bob sends:");
 		show (cipher);
-	
-<<<<<<< HEAD
-		System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));
 
-=======
-		System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));*/
->>>>>>> origin/Moore
+
+		System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));
 	}
 
 	public RSA()
 	{
-	
+
 	}
 
 	/**
@@ -66,22 +60,22 @@ public class RSA
 	 * @return long - The inverse of e (mod m)
 	 */
 	public static long inverse(long e, long m)
-	{	
+	{
 		//Remainder and quotient variable declarations (longs)
 		long r, q;
-		
+
 		//Arrays of longs to hold r, u, and v values from formula
 		long[] rLongs = new long[]{e, m}; //e=X, m=Y for x^(-1) (mod y)
 		long[] uLongs = new long[]{1, 0}; //U1, U2
 		long[] vLongs = new long[]{0, 1}; //V1, U2
-		
+
 		/*Storing the initial mod (long y) for multiplicative inverse operation at the end
 		long y = rLongs[1];*/
-		
+
 		do { //Formula R(n) = U(n)*X + V(n)*Y
 			r = rLongs[1] % rLongs[0]; //setting new remainder (r) from y/x [essentially r(n-2)/r(n-1)]
 			q = rLongs[1] / rLongs[0]; //setting new quotient (q) from r(n-2)/r(n-1)
-			
+
 			//Each step (n) => u(n) = u(n-2) - q(n)*u(n-1) && v(n) = v(n-2) - q(n)*v(n-1)
 			long u = uLongs[1] - avoidOverflow(uLongs[0], q, m); //Perform [q * uLongs[0] (mod m)], avoiding overflow
 			u = u % m;
@@ -91,7 +85,7 @@ public class RSA
 			v = v % m;
 			if (v < 0)
 				v = v + m;	//That final v value + the mod value, m (like multiplicative inverse, for if negative)
-			
+
 			//Update arrays of longs (r, u, and v) each step, n
 			rLongs[1] = rLongs[0];
 			rLongs[0] = r;
@@ -99,17 +93,17 @@ public class RSA
 			uLongs[0] = u;
 			vLongs[1] = vLongs[0];
 			vLongs[0] = v;
-			
+
 		} while (r != 1); //"until" remainder = 1
 		//Since the final u value * the final e value returns 1, the final u value is the inverse.
 		return uLongs[0]; //MULTIPLICATIVE INVERSE => vLongs[0] cancels out at end
 	}
-	
+
 	public static void show(long[] cipher)
 	{
-	
+
 	}
-	
+
 	/**
 	 * Raises a number (long), b, to the power p, modulo m.
 	 * @author Matt Moore
@@ -127,8 +121,8 @@ public class RSA
 		long[] nPowers = nPowers(b, nPower, m); //Saving computed powers of 2 to maxPower in array via call to helper method
 		nPower--;
 		long toNPower = (long) Math.pow(2, nPower); //Saving 2 to the nPower (2^(nPower)) as long => for use in decrementing iteration
-		
-		
+
+
 		for (int n = nPower; n>=0; n--) { //Decrementing iteration from max nPower to 0
 			if (power >= toNPower) { //Checking current input power, p, to the saved 2^(nPower), if greater subtract n by 1
 				power -= toNPower;
@@ -138,7 +132,7 @@ public class RSA
 		}
 	  return result;
 	}
-	
+
 	/**
 	 * Computes b^n for all powers of 2 to maxPower
 	 * @author Matt Moore
@@ -153,14 +147,14 @@ public class RSA
 		long[] nPowers = new long[maxPower];
 		long base = b;
 		nPowers[0] = b; //base case
-		
+
 		for (int i = 1; i <= maxPower; i++) { //Squares each base and stores it in return array
 			base = avoidOverflow(base, base, m); //Use this to avoid overflow in the modPower method.
 			nPowers[i] = base;
 		}
 		return nPowers;
 	}
-	
+
 	/**
 	 * Helper function performing multiplication on two n-bit values and avoid overflow
 	 * @author Matt Moore
@@ -170,25 +164,23 @@ public class RSA
 	 * @return result - long (mod m)
 	 */
 	public static long avoidOverflow(long x, long y, long m)
-	{ //Watch out for overflow. When multiplying two n-bit values, the result requires 2*n bits. 
+	{ //Watch out for overflow. When multiplying two n-bit values, the result requires 2*n bits.
 		long result = 0;
 		while (y > 0) { //Halfing max value of (longs) second num until 0
 			//If the second number is odd, add x (mod m) to result
 			if (y % 2 == 1)
 				result = (result + x) % m;
-			
+
 			x = (x * 2) % m; //Multiply first number by 2 (mod m)
 			y /= 2;
 		}
 		return result; //new quotient as result
 	}
-<<<<<<< HEAD
+
 	// Quentin Terry
-	public static long randPrime(int m, int n, java.util.Random rand)
-=======
-	
-	/**public static long randPrime(int m, int n, java.util.Random rand)
->>>>>>> origin/Moore
+
+public static long randPrime(int m, int n, java.util.Random rand)
+
 	{
 		rand = new Random();
 		int random = rand.nextInt(n - m) + m;
@@ -199,6 +191,7 @@ public class RSA
 		}
 		return random;
 	}
+
 	// Quentin Terry
 	public static boolean isPrime(int number) {
 		for (int i = 2; i < number; i++) {
@@ -208,6 +201,7 @@ public class RSA
 		}
 		return true;
 	}
+
 	// Quentin Terry
 	public static long relPrime(long n, java.util.Random rand)
 	{
@@ -220,18 +214,16 @@ public class RSA
 		}
 	  return findGCD(p, n % p);
 	}
-	
+
 	public static long toLong(java.lang.String msg, int p)
 	{
 	  return null;
 	}
-	
+
 	public static java.lang.String longTo2Chars(long x)
 	{
 	  return null;
 	}
 
-
-// ..........  place class methods here*/
 
 }
